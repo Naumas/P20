18c3a1e3a46969784ff00bb2135a23f421174efe
Naumas <78320740+Naumas@users.noreply.github.com>
2022-06-28 11:33:15 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,24 +2,26 @@
 var bgImg;
 var cat;
 var mouse;
-
+var bg
 var catImg1, catImg2, catImg3, mouseImg1, mouseImg2, mouseImg3;
 
 function preload() {
 
-bgImg = loadImage("images/garden.png");
-catImg1 = loadAnimation("images/cat1.png");
-mouseImg1 = loadAnimation("images/mouse1.png");
-catImg2 = loadAnimation("images/cat2.png, images/cat3.png");
-mouseImg2 = loadAnimation("images/mouse2.png, images/mouse3.png");
-catImg3 = loadAnimation("images/cat4.png")
-mouseImg3 = loadAnimation("images/mouse4.png")
+bgImg = loadImage("./images/garden.png");
+catImg1 = loadImage("./images/cat1.png");
+mouseImg1 = loadImage("./images/mouse1.png");
+catImg2 = loadAnimation("./images/cat2.png", "images/cat3.png");
+mouseImg2 = loadAnimation("./images/mouse2.png", "images/mouse3.png");
+catImg3 = loadImage("./images/cat4.png")
+mouseImg3 = loadImage("./images/mouse4.png")
 }
 
 function setup(){
     createCanvas(1000,800);
     //create tom and jerry sprites here
-cat = createSprite(870, 600)
+    bg = createSprite(500, 400);
+    bg.addImage("backgroundImage", bgImg);
+cat = createSprite(870, 600);
 cat.addAnimation("catSitting", catImg1)
 cat.scale = 0.2;
 mouse = createSprite(200, 600);
@@ -32,7 +34,7 @@ function draw() {
     background(255);
     //Write condition here to evalute if tom and jerry collide
 if(cat.x - mouse.x < (cat.width - mouse.width)/2){
-cat.velocityx = 0;
+cat.velocityX = 0;
 cat.addAnimation("catLastImage", catImg3);
 cat.changeAnimation("catLastImage");    
 cat.x = 300;
